3ab57f5b925be38e4c03e6acfd21162eb935fa62
Jakob Meier <dev.jakob.meier@gmail.com>
2020-03-17 16:03:00 +0100

    Update comcloudbot.js

--- comcloudbot.js
+++ comcloudbot.js
@@ -1,8 +1,2 @@
-const args = process.argv.slice(2);
 
-const client = args[0]
-const message = args[1]
-
-if (message.body === 'HiX') {
-      client.sendText(message.from, '👋 Hello from sulla!');
-    }
+console.log(message.from)
