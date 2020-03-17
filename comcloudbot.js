f7677123c6dfcff6c11a4505bd56169ac9f2ddeb
Jakob Meier <dev.jakob.meier@gmail.com>
2020-03-17 19:46:31 +0100

    removed !add

--- comcloudbot.js
+++ comcloudbot.js
@@ -6,17 +6,15 @@
       cmd[]
 */
 
+if(data.store==undefined) data.store = {};
+
 var wiki = [
-      `Wiki 1`,`Wiki 2`
+      `Wiki 1\ntest`,`Wiki 2`
 ];
 
  cmd = message.body.split(" ");
 
 switch (cmd[0]) {
-      case "!add":
-            data.store+=cmd[1]
-            client.sendText(message.from,data.store);
-            break;
       case "!help":
             let page = (cmd[1]>wiki.length)?1:cmd[1];
             client.sendText(message.from,wiki[page - 1]);
